05f4fc081281f4ff84aacd71438f9f5aada5eed0
Ha Vuong <havuong138@gmail.com>
2020-04-12 16:31:40 +0700

    Using a DropDownButton and DropDownItems

--- trip_cost_app/lib/main.dart
+++ trip_cost_app/lib/main.dart
@@ -22,6 +22,8 @@ class HelloYou extends StatefulWidget {
 
 class _HelloYouState extends State<HelloYou> {
   String name = '';
+  final _currencies = ['Dollars', 'Euro', 'Pounds'];
+  String _currency = 'Dollars';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,18 +36,33 @@ class _HelloYouState extends State<HelloYou> {
           child: Column(
             children: <Widget>[
               TextField(
-                decoration: InputDecoration(
-                  hintText: 'Please insert your name'
-                ),
+                decoration:
+                    InputDecoration(hintText: 'Please insert your name'),
                 onChanged: (String string) {
                   setState(() {
                     name = string;
                   });
                 },
               ),
+              DropdownButton<String>(
+                items: _currencies.map((String value) {
+                  return DropdownMenuItem<String>(
+                      value: value, child: Text(value));
+                }).toList(),
+                value: _currency,
+                onChanged: (String value) {
+                  _onDropdownChanged(value);
+                },
+              ),
               Text('Hello ' + name + '!')
             ],
           )),
     );
+  }
+
+  _onDropdownChanged(String value) {
+    setState(() {
+      this._currency = value;
+    });
   }
 }
