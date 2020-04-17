35b22285400e5a5c5e9983a691486e8a4f84a8c1
Ha Vuong <havuong138@gmail.com>
2020-04-17 09:31:04 +0700

    Navigator and Route

--- words_list/lib/main.dart
+++ words_list/lib/main.dart
@@ -13,58 +13,82 @@ class RandomEnglishWords extends StatefulWidget {
 }
 
 class RandomEnglishWordsState extends State<RandomEnglishWords> {
-  final _words = <WordPair>[];
-  final _checkedWords = new Set<WordPair>();
+  final List<WordPair> _words = <WordPair>[];
+  final Set<WordPair> _saved = new Set<WordPair>();
+  final _biggerFont = new TextStyle(fontSize: 20, color: Colors.grey);
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('List of English words'),
+        actions: <Widget>[
+          new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved)
+        ],
       ),
-      body: new ListView.builder(itemBuilder: (context, index) {
-        if (index >= _words.length) {
-          _words.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_words[index], index);
-      }),
+      body: Center(
+        child: ListView.builder(itemBuilder: (context, index) {
+          if (index >= _words.length) {
+            _words.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_words[index], index);
+        }),
+      ),
     );
   }
 
   Widget _buildRow(WordPair wordPair, int index) {
-    final textColor = index % 2 == 0 ? Colors.red : Colors.blue;
-    final isChecked = _checkedWords.contains(wordPair);
+    final bool isSaved = _saved.contains(wordPair);
     return new ListTile(
-        leading: new Icon(
-            isChecked ? Icons.check_box : Icons.check_box_outline_blank,
-            color: textColor),
         title: new Text(
           wordPair.asPascalCase,
-          style: new TextStyle(fontSize: 20, color: textColor),
+          style: _biggerFont,
+        ),
+        trailing: Icon(
+          isSaved ? Icons.favorite : Icons.favorite_border,
+          color: Colors.red,
         ),
         onTap: () {
           setState(() {
-            if (isChecked) {
-              _checkedWords.remove(wordPair);
+            if (isSaved) {
+              _saved.remove(wordPair);
             } else {
-              _checkedWords.add(wordPair);
+              _saved.add(wordPair);
             }
           });
         });
+  }
+
+  void _pushSaved() {
+    Navigator.of(context)
+        .push(new MaterialPageRoute(builder: (BuildContext context) {
+      final Iterable<ListTile> tiles = _saved.map((WordPair pair) {
+        return new ListTile(
+          title: new Text(
+            pair.asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      });
+      final List<Widget> divided =
+          ListTile.divideTiles(tiles: tiles, context: context).toList();
+      return Scaffold(
+          appBar: AppBar(
+            title: const Text("Saved list"),
+          ),
+          body: new ListView(
+            children: divided,
+          ));
+    }));
   }
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // final wordPair = new WordPair.random();
     return MaterialApp(
       title: 'Words List',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Flutter Demo Home Page'),
-        ),
-        body: new Center(child: new RandomEnglishWords()),
-      ),
+      home: RandomEnglishWords(),
     );
   }
 }
