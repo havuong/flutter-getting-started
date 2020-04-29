a4f3ba8254fa046b65ca40dd55efb0f1b8b8089a
Ha Vuong <havuong138@gmail.com>
2020-04-29 21:39:10 +0700

    Integrate firebase

---
+++ taxi_app/lib/src/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'ui/login_page.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginPage(),
+    );
+  }
+}
--- taxi_app/lib/src/ui/register_page.dart
+++ taxi_app/lib/src/ui/register_page.dart
@@ -1,1 +1,150 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
 
+import 'login_page.dart';
+
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+        constraints: BoxConstraints.expand(),
+        color: Colors.white,
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 140,
+              ),
+              Image.asset('ic_car_red.png'),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 40, 0, 6),
+                child: Text(
+                  "Welcome Aboard!",
+                  style: TextStyle(fontSize: 22, color: Color(0xff333333)),
+                ),
+              ),
+              Text(
+                "Signup with iCab in simple steps",
+                style: TextStyle(fontSize: 16, color: Color(0xff606470)),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 50, 0, 20),
+                child: StreamBuilder(
+                    builder: (context, snapshot) => TextField(
+                          style: TextStyle(fontSize: 18, color: Colors.black),
+                          decoration: InputDecoration(
+                              errorText:
+                                  snapshot.hasError ? snapshot.error : null,
+                              labelText: "Name",
+                              prefixIcon: Container(
+                                  width: 50, child: Image.asset("ic_user.png")),
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Color(0xffCED0D2), width: 1),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(6)))),
+                        )),
+              ),
+              StreamBuilder(
+                  builder: (context, snapshot) => TextField(
+                        style: TextStyle(fontSize: 18, color: Colors.black),
+                        decoration: InputDecoration(
+                            labelText: "Phone Number",
+                            errorText:
+                                snapshot.hasError ? snapshot.error : null,
+                            prefixIcon: Container(
+                                width: 50, child: Image.asset("ic_phone.png")),
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Color(0xffCED0D2), width: 1),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(6)))),
+                      )),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                child: StreamBuilder(
+                    builder: (context, snapshot) => TextField(
+                          style: TextStyle(fontSize: 18, color: Colors.black),
+                          decoration: InputDecoration(
+                              labelText: "Email",
+                              errorText:
+                                  snapshot.hasError ? snapshot.error : null,
+                              prefixIcon: Container(
+                                  width: 50, child: Image.asset("ic_mail.png")),
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Color(0xffCED0D2), width: 1),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(6)))),
+                        )),
+              ),
+              StreamBuilder(
+                  builder: (context, snapshot) => TextField(
+                        obscureText: true,
+                        style: TextStyle(fontSize: 18, color: Colors.black),
+                        decoration: InputDecoration(
+                            errorText:
+                                snapshot.hasError ? snapshot.error : null,
+                            labelText: "Password",
+                            prefixIcon: Container(
+                                width: 50, child: Image.asset("ic_lock.png")),
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Color(0xffCED0D2), width: 1),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(6)))),
+                      )),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 30, 0, 40),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 52,
+                  child: RaisedButton(
+                    onPressed: _onSignUpClicked,
+                    child: Text(
+                      "Signup",
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    ),
+                    color: Color(0xff3277D8),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(6))),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+                child: RichText(
+                  text: TextSpan(
+                      text: "Already a User? ",
+                      style: TextStyle(color: Color(0xff606470), fontSize: 16),
+                      children: <TextSpan>[
+                        TextSpan(
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => LoginPage()));
+                              },
+                            text: "Login now",
+                            style: TextStyle(
+                                color: Color(0xff3277D8), fontSize: 16))
+                      ]),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _onSignUpClicked() {}
+}
